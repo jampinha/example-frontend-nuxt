0749179409d41f7ef6011c0464abce9685cada92
Mario Bandeira <josemariobandeira@hotmail.com>
2023-12-14 13:12:14 -0300

    setup: sass style setup

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <FirstComponent></FirstComponent>
   </div>
 </template>
---
+++ components/FirstComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="first-component">First Componnet</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.first-component {
+  background-color: $test-color;
+}
+</style>
